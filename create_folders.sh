177a5118a9eda6f7aaa6c7a0af8e723d794ea934
Théo <theo.vincent@eleves.enpc.fr>
2021-07-13 09:39:55 +0000

    Create empty folders

---
+++ create_folders.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir data/MI01A_Preprocessing_main
+
+mkdir data/MI01B_Preprocessing_imagesIDs
+
+mkdir data/MI01C_Preprocessing_folds
+
+mkdir data/MI03B_Predictions_concatenate
+
+mkdir data/MI03C_Predictions_merge
+
+mkdir data/MI03D_Predictions_eids
+mkdir data/MI03D_Predictions_eids/Predictions_eids_concatenate
+
+mkdir data/MI04A_Performances_generate
+
+mkdir data/MI04B_Performances_merge
+
+mkdir data/MI04C_Performances_tuning
+
+mkdir data/MI05A_Ensembles_predictions
+
+mkdir data/MI05B_Performances_generate
+
+mkdir data/MI05C_Performances_merge
+
+mkdir data/MI06A_Residuals_generate
+
+mkdir data/MI06B_Residuals_correlations
+
+mkdir data/MI07A_Select_best
